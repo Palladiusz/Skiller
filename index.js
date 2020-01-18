ec1723ffbda9099c55d712a85822fe314ce85368
Palladiusz <palladiusz.ra@gmail.com>
2020-01-18 15:29:04 +0100

    progress bar add

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const timerElement = document.querySelector(".timer");
 const inputMinutes = document.querySelector(".inputTime");
+const progressBar = document.querySelector(".progress");
+
 var seconds, minutes, time;
 var timer;
+const finishTime = 1*60
 time = (parseInt(inputMinutes.value) * 60);
 minutes = Math.round(time / 60);
 seconds = Math.round(time % 60);
@@ -17,16 +20,25 @@ function startTimer() {
 }
 
 function run() {
+    let totalTimeLeft = seconds + (minutes*60);
+    let percentOfTime = ((totalTimeLeft / finishTime)*100).toFixed(0);
+    setTimer(minutes, seconds);
     seconds++;
 
     if (seconds === 60) {
         seconds = 0;
         minutes++;
     };
-    if (minutes === 1 && seconds === 00) {
+
+    if (finishTime === totalTimeLeft) {
+        progressBar.style.setProperty("width", `100%`);
         stopTimer();
+
     };
-    setTimer(minutes, seconds);
+    progressBar.style.setProperty("width", `${percentOfTime}%`);
+    document.querySelector(".percent").textContent = `${percentOfTime}%`;
+
+
 }
 
 function stopTimer() {
@@ -38,6 +50,7 @@ function resetTimer() {
     minutes = Math.round(time / 60);
     seconds = Math.round(time % 60);
     setTimer(minutes, seconds);
+    progressBar.style.setProperty("width", `0%`)
 }
 
 
